107cba3d7947654c56d9ee99d42b19d510e66370
broski-wz-hia <triomo1239@gmail.com>
2026-07-10 17:33:18 +0100

    Update guildBanAdd.js

--- src/events/guildBanAdd.js
+++ src/events/guildBanAdd.js
@@ -5,20 +5,13 @@ export default {
   name: Events.GuildBanAdd,
 
   async execute(ban) {
+    console.log("GuildBanAdd event fired for:", ban.user.tag);
+
     try {
-      await ban.user.send(`
-You have been banned from **${ban.guild.name}**.
-
-If you believe this was a mistake, you may appeal here:
-
-https://YOUR-APPEAL-LINK
-
-Please include:
-• Your Discord username
-• Why you think the ban should be removed
-• Any evidence that supports your appeal
-`);
+      await ban.user.send("Test DM");
+      console.log("DM sent successfully!");
     } catch (error) {
+      console.error(error);
       logger.warn(`Could not DM banned user ${ban.user.tag}`);
     }
   },
