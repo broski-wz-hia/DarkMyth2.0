9425495cdc436d8a88283776f762d90cebeb12dd
broski-wz-hia <triomo1239@gmail.com>
2026-07-10 17:51:12 +0100

    Update guildBanAdd.js

--- src/events/guildBanAdd.js
+++ src/events/guildBanAdd.js
@@ -8,11 +8,14 @@ export default {
     console.log("GuildBanAdd event fired for:", ban.user.tag);
 
     try {
-      await ban.user.send("You have been banned from Just Us Games. You probably broke one of the rules. DM the Community Manager myth1xaldev or appeal here https://discord.gg/KhJDcuw848");
+      await ban.user.send({
+        content: "You have been banned from **Just Us Games**.\n\nYou likely violated one or more of our server rules.\n\nIf you believe this was a mistake, you can appeal here:\nhttps://discord.gg/KhJDcuw848\n\nAlternatively, you may contact **myth1xaldev** if appropriate."
+      });
+
       console.log("DM sent successfully!");
     } catch (error) {
       console.error(error);
-      logger.warn(`Could not DM banned user ${ban.user.tag}`);
+      logger.warn(`Could not DM banned user ${ban.user.tag}: ${error.message}`);
     }
   },
 };
